408d916be2e979b6b102254107221696274d6232
Alex Tello <alex.tello@esedsl.com>
2023-11-26 07:42:20 +0000

    fix(navbar & home): responsiv navbar and home

--- src/app/(app)/(dashboard)/page.tsx
+++ src/app/(app)/(dashboard)/page.tsx
@@ -8,8 +8,8 @@ import { StandardButton } from '@/components/Buttons/StandardButton';
 export default function Page() {
   return (
     <>
-      <div className='mx-72 items-center text-center'>
-        <h1 className='mb-10 text-white text-5xl p-4 font-bold mt-9 '>
+      <div className='mx-6 md:mx-12 lg:mx-24 xl:mx-36 items-center text-center'>
+        <h1 className='mb-6 md:mb-8 lg:mb-10 text-white text-3xl md:text-4xl lg:text-5xl p-2 md:p-3 lg:p-4 font-bold mt-6 md:mt-8 lg:mt-9'>
           Descobreix la millor manera de cuidar les teves plantes
         </h1>
 
@@ -21,7 +21,7 @@ export default function Page() {
           height={500}
         />
         <div className='h-2' />
-        <div className='flex justify-center space-x-10'>
+        <div className='flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4'>
           <Link href='/home'>
             <StandardButton text='Anar al teu hort'>
               <IoMdArrowForward className='ml-2' />
@@ -33,11 +33,12 @@ export default function Page() {
             </StandardButton>
           </Link>
         </div>
-        <div className='h-10' />
+        <div className='h-6 md:h-10' />
       </div>
+
       <div className='w-full bg-light-green-slide'>
-        <div className='mx-80 items-center text-center flex py-10'>
-          <div className='w-1/2'>
+        <div className='mx-6 md:mx-12 lg:mx-24 xl:mx-36 items-center text-center flex flex-col md:flex-row py-6 md:py-10'>
+          <div className='w-full md:w-1/2'>
             <Image
               className='mx-auto my-auto'
               src='/images/home/farmer1.png'
@@ -46,22 +47,23 @@ export default function Page() {
               height={500}
             />
           </div>
-          <div className='w-1/2 p-4'>
-            <p className='mb-10 text-green-nav text-3xl p-4 font-bold mt-9'>
+          <div className='w-full md:w-1/2 p-4'>
+            <p className='mb-6 md:mb-8 lg:mb-10 text-green-nav text-lg md:text-3xl p-2 md:p-3 lg:p-4 font-bold mt-6 md:mt-8 lg:mt-9'>
               Afegeix les teves precioses plantes i descobreix quines són les
               seves necessitats
             </p>
           </div>
         </div>
       </div>
+
       <div className='w-full'>
-        <div className='mx-80 items-center text-center flex py-10'>
-          <div className='w-1/2 p-4'>
-            <p className='mb-10 text-white text-3xl p-4 font-bold mt-9'>
+        <div className='mx-6 md:mx-12 lg:mx-24 xl:mx-36 items-center text-center flex flex-col md:flex-row py-6 md:py-10'>
+          <div className='w-full md:w-1/2 p-4'>
+            <p className='mb-6 md:mb-8 lg:mb-10 text-white text-lg md:text-3xl p-2 md:p-3 lg:p-4 font-bold mt-6 md:mt-8 lg:mt-9'>
               Descobriràs com i quan regar-les
             </p>
           </div>
-          <div className='w-1/2'>
+          <div className='w-full md:w-1/2'>
             <Image
               className='mx-auto my-auto'
               src='/images/home/farmer2.png'
@@ -72,9 +74,10 @@ export default function Page() {
           </div>
         </div>
       </div>
+
       <div className='w-full bg-light-green-slide'>
-        <div className='mx-80 items-center text-center flex py-10'>
-          <div className='w-1/2'>
+        <div className='mx-6 md:mx-12 lg:mx-24 xl:mx-36 items-center text-center flex flex-col md:flex-row py-6 md:py-10'>
+          <div className='w-full md:w-1/2'>
             <Image
               className='mx-auto my-auto'
               src='/images/home/planta.png'
@@ -83,16 +86,17 @@ export default function Page() {
               height={400}
             />
           </div>
-          <div className='w-1/2 p-4'>
-            <p className='mb-10 text-green-nav text-3xl p-4 font-bold mt-9'>
+          <div className='w-full md:w-1/2 p-4'>
+            <p className='mb-6 md:mb-8 lg:mb-10 text-green-nav text-lg md:text-3xl p-2 md:p-3 lg:p-4 font-bold mt-6 md:mt-8 lg:mt-9'>
               Podràs seguir el seu creixement i quan et donarà els seus fruits
             </p>
           </div>
         </div>
       </div>
+
       <div className='w-full '>
-        <div className='mx-80 items-center text-center py-10'>
-          <h1 className='mb-10 text-white text-5xl p-4 font-bold mt-9 '>
+        <div className='mx-6 md:mx-12 lg:mx-24 xl:mx-36 items-center text-center py-6 md:py-10'>
+          <h1 className='mb-6 md:mb-8 lg:mb-10 text-white text-3xl md:text-4xl lg:text-5xl p-2 md:p-3 lg:p-4 font-bold mt-6 md:mt-8 lg:mt-9'>
             Si encara no tens un hort, crea&apos;l ara gratuïtament!
           </h1>
           <Link href='/signup'>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,24 +2,29 @@
 
 import { croissant } from '@/lib/fonts';
 import { UserMenu } from '@/components/UserMenu';
+import { usePathname } from 'next/navigation';
 
 export const NavBar = () => {
+  const path = usePathname();
+
   return (
     <nav className='border-gray-200 bg-green-nav'>
       <div
-        className={`${croissant.className} flex flex-wrap justify-between items-center mx-10 p-4 `}
+        className={`${croissant.className} flex flex-wrap justify-between items-center mx-6 md:mx-12 lg:mx-24 xl:mx-36 p-2 md:p-3 lg:p-4`}
       >
         <a
           href='/home'
           className='flex items-center space-x-3 rtl:space-x-reverse'
         >
-          <p className='text-white self-center text-5xl whitespace-nowrap'>
+          <p className='text-white self-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl whitespace-nowrap'>
             My little garden
           </p>
         </a>
-        <div className='flex items-center space-x-6 rtl:space-x-reverse cursor-pointer'>
-          <UserMenu />
-        </div>
+        {path !== '/' && (
+          <div className='flex items-center space-x-4 md:space-x-6 rtl:space-x-reverse cursor-pointer'>
+            <UserMenu />
+          </div>
+        )}
       </div>
     </nav>
   );
